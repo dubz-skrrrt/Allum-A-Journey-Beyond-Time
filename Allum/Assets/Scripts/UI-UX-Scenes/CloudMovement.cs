b50df9aa0eba2ed79f1d4e30eba2da33cc7be68d
dubz-skrrrt <chill.99.lr@gmail.com>
2022-02-10 21:51:03 +0800

    CloudMovement

---
+++ Allum/Assets/Scripts/UI-UX-Scenes/CloudMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    private const float XCloudPosDestroy = 15f;
+    private const float cloudMoveSpeed = 0.2f;
+    public List<Transform> cloudList;
+    private void Awake()
+    {
+        foreach (Transform cloud in this.gameObject.GetComponentInChildren<Transform>())
+        {
+            cloudList.Add(cloud);
+        }
+    }
+    private void Update()
+    {
+        cloudMovement();
+    }
+
+    private void cloudMovement()
+    {
+        foreach (Transform clouds in cloudList)
+        {
+            clouds.position += new Vector3(1, 0, 0) * cloudMoveSpeed * Time.deltaTime;
+            float RightMostXPos = -16f;
+            
+            if (clouds.position.x > XCloudPosDestroy)
+            {
+                for (int i = 0; i < cloudList.Count; i++)
+                {
+                    if (cloudList[i].position.x < RightMostXPos)
+                    {
+                        RightMostXPos = cloudList[i].position.x;
+                    }
+                }
+
+                float cloudwidthHalf = 4f;
+                clouds.position = new Vector3(RightMostXPos + cloudwidthHalf, clouds.position.y, clouds.position.z);
+            }
+        }
+    }
+}
