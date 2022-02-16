f2c572a598a4820b7494cc0afac7b58e6925d2f6
dubz-skrrrt <chill.99.lr@gmail.com>
2022-02-16 22:54:35 +0800

    MainMenu
    -Added Animations on objects
    -New Button Design

--- Allum/Assets/Scripts/Managers/GameManager.cs
+++ Allum/Assets/Scripts/Managers/GameManager.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.EventSystems;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public GameState state;
+    public GameObject defaultSelection;
     public static event Action<GameState> OnGameStateChanged;
 
     void Awake()
     {
         Instance = this;
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(defaultSelection);
+        }
+    }
+
     public void UpdateGameState(GameState newState)
     {
         state = newState;
