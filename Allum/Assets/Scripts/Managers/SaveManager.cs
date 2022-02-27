81f69ad7c554be21ea49621202e9b64ea6f29610
dubz-skrrrt <chill.99.lr@gmail.com>
2022-02-27 22:36:11 +0800

    Revert "save file on playmode"
    This reverts commit 40fec20da34d11509e4d75f7e2fb27cadc0ab4e3.

--- Allum/Assets/Scripts/Managers/SaveManager.cs
+++ Allum/Assets/Scripts/Managers/SaveManager.cs
@@ -24,11 +24,6 @@ public class SaveManager : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
         if (!File.Exists(Application.persistentDataPath +  "/SaveFile_" + SaveSlotData.SlotName + ".dat"))
         {
-            //save file on playmode in Unity
-            if (SaveSlotData.SlotName == null)
-            {
-                SaveSlotData.SlotName = "test";
-            }
             Debug.Log(SaveSlotData.SlotName);
             SceneSwitchData();
             SavePlayer();
